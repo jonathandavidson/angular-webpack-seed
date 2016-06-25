6e8a2b5f1e5e98ce20750c76b6dcf313e8820c98
Jonathan Davidson <jdavidson@netsplasher.com>
2016-06-25 16:20:27 -0500

    Added production build optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
-module.exports = {
+const webpack = require('webpack');
+
+const config = {
+  devtool: 'eval',
   entry: './src/app.js',
   module: {
     loaders: [
       {
+        include: /src/,
         loaders: ["style", "css", "sass"],
         test: /\.scss$/
       },
       {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
+        include: /src/,
+        loader: 'babel',
         test: /\.js$/
       }
     ]
@@ -16,5 +20,23 @@ module.exports = {
   output: {
     filename: 'app.bundle.js',
     path: './public/js'
-  }
+  },
+  plugins: []
 };
+
+if (process.env.NODE_ENV === 'production') {
+    config.devtool = 'cheap-module-source-map';
+
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+        },
+        output: {
+          comments: false,
+        }
+      })
+    );
+}
+
+module.exports = config;
